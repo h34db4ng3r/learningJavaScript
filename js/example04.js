7e2e1dc4c368437a1681d9d47ba3892cd566c547
Fredrick Børseth <134796123+h34db4ng3r@users.noreply.github.com>
2023-09-10 20:59:55 +0200

    Add files via upload

---
+++ js/example04.js
@@ -0,0 +1,22 @@
+var table = 3;                  // Unit of table
+var operator = 'addition';      // Type of calculation (defaults to addition)
+var i = 1;                      // Set counter to 1
+var msg = '';                   // Message
+
+if (operator === 'addition') {          // If the operator variable says addition
+    while (i < 11) {                    // While counter is less than 11
+        msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';  //calculation
+        i++;                            // Add 1 to counter
+    }
+} else {                                // Otherwise
+    while (i < 11) {                    // While counter is less than 11
+        msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';  //calculation
+        i++;                            // add 1 to counter
+
+    }
+}
+
+// Write the message into the page
+
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
