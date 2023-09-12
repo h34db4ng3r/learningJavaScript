9ce9ca8b3b202cf8182c63779adbb3e33d6c2686
Fredrick Børseth <134796123+h34db4ng3r@users.noreply.github.com>
2023-09-13 00:48:41 +0200

    Add files via upload

---
+++ dom/app.js
@@ -0,0 +1,28 @@
+// DOM Manipulation
+
+
+
+// getElementById()
+
+const title = document.getElementById('main-heading');
+console.log(title);
+
+// getElementByClassName()
+
+const listItem = document.getElementsByClassName('list-items');
+console.log(listItem);
+
+// getElementsByTagName
+
+const listItemTag = document.getElementsByTagName('li');
+console.log(listItemTag);
+
+// querySelector()
+
+const container = document.querySelector('div');
+console.log(container);
+
+// querySelectorAll()
+
+const containerAll = document.querySelectorAll('div');
+console.log(container);
